7d431679f06c07b4408753bb728e10b3e528ec79
Alex <galalex20160925@gmail.com>
2024-11-04 18:55:48 +0100

    function atrendezes

--- functions.js
+++ functions.js
@@ -20,3 +20,106 @@ function createhtmlelementparentid(tag,id,parentid){
     }
     
 }
+
+
+function rendertable(){
+    let tablebody =document.getElementById('persontbody')
+    for(const person of array){
+        const tr = document.createElement('tr')
+    
+        const lastname = document.createElement('td')
+        tablebody.appendChild(tr)
+        tr.appendChild(lastname)
+        lastname.innerHTML = person.lastname
+    
+        const firstname = document.createElement('td')
+        tablebody.appendChild(tr)
+        tr.appendChild(firstname)
+        firstname.innerHTML = person.firstname1
+        
+        
+        
+        
+        tablebody.appendChild(tr)
+        
+    
+        if(person.firstname2===undefined){
+            firstname.colSpan = 2
+        }
+        else{
+            const firstname2 = document.createElement('td')
+            firstname2.innerHTML = person.firstname2
+            tr.appendChild(firstname2)
+    
+        }
+        tr.addEventListener('click',function(e){
+            console.log('click')
+            
+            const select=tablebody.querySelector('.selected')
+                if(select!=undefined){
+                    select.classList.remove('selected')
+                    
+                }e.currentTarget.classList.add('selected')
+    
+                
+        })
+        
+        const hazassag = document.createElement('td')
+                tablebody.appendChild(tr)
+                tr.appendChild(hazassag)
+        hazassag.innerHTML=person.married
+        const allat = document.createElement('td')
+                tablebody.appendChild(tr)
+                tr.appendChild(allat)
+        allat.innerHTML=person.pet
+        if(hazassag.innerHTML==="true"){
+            hazassag.innerHTML="igen"
+        }else {
+            hazassag.innerHTML="nem"
+        }
+        
+    }
+}
+
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+    const lastname =document.getElementById('lastname')
+    const firstname1 =document.getElementById('firstname1')
+    const firstname2 =document.getElementById('firstname2')
+    const married =document.getElementById('married')
+    const pet =document.getElementById('pet')
+    const lastmamevalue =lastname.value
+    const firstname1value = firstname1.value
+    const firstname2value = firstname2.value
+    const marriedchecked = married.checked
+    const petvalue = pet.value
+
+    
+    
+
+
+
+
+
+const emberek={
+    lastname : lastmamevalue,
+    firstname1: firstname1value,
+    firstname2: firstname2value,
+    married : marriedchecked,
+    pet: petvalue
+}
+
+if (validatefields(lastname,firstname1,pet)){
+    let tablebody =document.getElementById('persontbody')
+    array.push(emberek)
+    console.log(array)
+    tablebody.innerHTML=""
+    rendertable()
+    form.reset()
+}
+})
+
+
+
+
--- main.js
+++ main.js
@@ -38,7 +38,7 @@ let valami = [
 
 createHtmlElement("table","persontable",document.body)
 createhtmlelementparentid("thead", "personthead", "persontable");
-createhtmlelementparentid("tablebody", "persontbody", "personthead");
+createhtmlelementparentid("tbody", "persontbody", "personthead");
 createhtmlelementparentid("tr", "persontr", "persontbody");
 
 
@@ -61,103 +61,10 @@ for (let i =0;i<valami.length;i++){
 rendertable()
 
 
-function rendertable(){
-    for(const person of array){
-        const tr = document.createElement('tr')
-    
-        const lastname = document.createElement('td')
-        tablebody.appendChild(tr)
-        tr.appendChild(lastname)
-        lastname.innerHTML = person.lastname
-    
-        const firstname = document.createElement('td')
-        tablebody.appendChild(tr)
-        tr.appendChild(firstname)
-        firstname.innerHTML = person.firstname1
-        
-        
-        
-        
-        tablebody.appendChild(tr)
-        
-    
-        if(person.firstname2===undefined){
-            firstname.colSpan = 2
-        }
-        else{
-            const firstname2 = document.createElement('td')
-            firstname2.innerHTML = person.firstname2
-            tr.appendChild(firstname2)
-    
-        }
-        tr.addEventListener('click',function(e){
-            console.log('click')
-            
-            const select=tablebody.querySelector('.selected')
-                if(select!=undefined){
-                    select.classList.remove('selected')
-                    
-                }e.currentTarget.classList.add('selected')
-    
-                
-        })
-        
-        const hazassag = document.createElement('td')
-                tablebody.appendChild(tr)
-                tr.appendChild(hazassag)
-        hazassag.innerHTML=person.married
-        const allat = document.createElement('td')
-                tablebody.appendChild(tr)
-                tr.appendChild(allat)
-        allat.innerHTML=person.pet
-        if(hazassag.innerHTML==="true"){
-            hazassag.innerHTML="igen"
-        }else {
-            hazassag.innerHTML="nem"
-        }
-        
-    }
-}
-
 
 
 const form = document.getElementById('form')
-form.addEventListener('submit',function(e){
-    e.preventDefault()
-    const lastname =document.getElementById('lastname')
-    const firstname1 =document.getElementById('firstname1')
-    const firstname2 =document.getElementById('firstname2')
-    const married =document.getElementById('married')
-    const pet =document.getElementById('pet')
-    const lastmamevalue =lastname.value
-    const firstname1value = firstname1.value
-    const firstname2value = firstname2.value
-    const marriedchecked = married.checked
-    const petvalue = pet.value
 
-    
-    
-
-
-
-
-
-const emberek={
-    lastname : lastmamevalue,
-    firstname1: firstname1value,
-    firstname2: firstname2value,
-    married : marriedchecked,
-    pet: petvalue
-}
-
-if (validatefields(lastname,firstname1,pet)){
-    array.push(emberek)
-    console.log(array)
-    tablebody.innerHTML=""
-    rendertable()
-    form.reset()
-}
-})
 function validatefields(lastname,firstname,pet){
     let result = true;
     if (lastname.value===""){
